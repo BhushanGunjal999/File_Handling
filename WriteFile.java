6a11133899ba1372d43f35df0d63495b75cef266
BhushanGunjal999 <bhushangunjal4258@gmail.com>
2024-08-24 22:14:44 +0530

    Add files via upload

---
+++ WriteFile.java
@@ -0,0 +1,17 @@
+
+import java.io.FileOutputStream;
+
+
+class WriteFile
+{
+    public static void main(String Arg[]) throws Exception 
+    {
+        FileOutputStream fobj = new FileOutputStream("Infosystems.txt");
+        String str = "India is my country";
+
+        byte Arr[] = str.getBytes();
+        fobj.write(Arr);
+        fobj.close();
+        
+    }
+}
